51867f66baa0a8c2eb8a1fced8be41b34cee8b5d
Henry-Julian <hjqg689@gmail.com>
2023-02-18 19:14:40 -0500

    Taller_03_15_02_2023_Punto4

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// 3.	Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado.
+// 4.	Realizar un programa de procesos en el que se almacenen 3 números en 3 variables A, B y C.
+// El diagrama debe decidir cual es el mayor y cual es el menor
 
-let num1 = Number(prompt("Señor usuario, ingrese el primer número"));
-let num2 = Number(prompt("Señor usuario, ingrese el segundo número"));
+let A = Number(prompt("Señor usuario, ingrese el valor para asignarle a la variable A:"));
+let B = Number(prompt("Señor usuario, ingrese el valor para asignarle a la variable B:"));
+let C = Number(prompt("Señor usuario, ingrese el valor para asignarle a la variable C:"));
 
-console.log(`Señor usuario, la suma de los 2 números ingresados es: ${num1+num2}`)
-
-
+console.log(`Señor usuario, el número mayor de las tres variables es: ${Math.max(A,B,C)}`)
+console.log(`Señor usuario, el número menor de las tres variables es: ${Math.min(A,B,C)}`)
 
