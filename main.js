bfed21419f5b041bda02b5173f2cda940ded1f10
Henry-Julian <hjqg689@gmail.com>
2023-02-18 19:58:43 -0500

    Taller_03_15_02_2023_Punto6

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-// 5.	Realizar un programa que permita escribir los 100 primeros pares.
+// 6.	Realizar un programa que lea N números, calcule y escriba la suma de los pares y el producto de los impares.
 
-let par
-console.log("A continuación podrá observar los 100 primeros números pares desde el 2 hasta el 100: ");
-for(par=2; par <=100; par+=2){
-    console.log(`${par}`)
-} 
+let num, par=0, impar=1; 
 
+do
+{num = Number(prompt("Señor usuario, por favor ingrese un número. Posteriormente le da aceptar para seguir ingresando más números y cuando desee terminar presione el número 0"));
+if(num == 0){
+    console.log("El programa ha terminado.")
+}
+else{
+if(num%2 == 0){
+    par+=num;
+    console.log(`El número par ingresado es ${num} y la suma del número par escrito anteriormente es de: ${par}`);
+}
+else{
+    impar*=num;
+    console.log(`El número impar ingresado es ${num} y la multiplicación del número impar escrito anteriormente con este es de: ${impar}`);
+}
+}}
+while(num != 0 ){
+}
 
 
